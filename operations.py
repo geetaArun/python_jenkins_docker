80b1f6edfdb60cc6ccd997e6637bd9c6cd1d86ce
Geeta <geeta@BTs-MacBook-Pro.local>
2023-11-13 17:41:56 +0530

    A new math operations module

---
+++ operations.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+print("sum of two numbers is:", add(10,20));
+print("subtraction of 2 numbers is:", subtract(30,10));
+print("multiplication of two numbers is:", multiply(10,20));
+print("Division of two numbers is:", divide(20,10));
