34320a228ddb4c0bbcf82e7fad249ab1d5578edd
Geeta <geeta@BTs-MacBook-Pro.local>
2023-11-14 01:01:50 +0530

    py updated

--- operations.py
+++ operations.py
@@ -14,3 +14,4 @@ print("sum of two numbers is:", add(10,20));
 print("subtraction of 2 numbers is:", subtract(30,10));
 print("multiplication of two numbers is:", multiply(10,20));
 print("Division of two numbers is:", divide(20,10));
+print("Project Completed!")
